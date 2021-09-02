c898f0ab31f3d27ade91c0a334ae54888382b199
Christina Dietrich <christina.dietrich@my.simpson.edu>
2021-09-02 15:21:28 -0500

    Work on lab 1

---
+++ Lab 01 - First Program/lab_01.py
@@ -0,0 +1,4 @@
+print("Professor Craven says, \"Do not capitalize print!\".")
+print("I want to go back and take a nap,\nhowever, I cannot leave.")
+print("I also want to eat dinner right now. The problem is that Pfieffer does not always have good food.")
+print("I do not want to do the homework that I have tonight.")
