4d92e63b4d6a9c8c286d13079a76f13ea97aa619
Christina Dietrich <christina.dietrich@my.simpson.edu>
2021-09-23 12:02:40 -0500

    Work on lab 4

---
+++ Lab 04 - Camel/lab_04.py
@@ -0,0 +1,106 @@
+def main():
+    print("Welcome to Popeye!")
+    print("You, Popeye, have stolen Bluto's camel to make your way across the Mobi desert.")
+    print("Bluto wants his camel back and is chasing you down!")
+    print("Survive your desert trek and out run Bluto.")
+
+    # All of the variables
+    user_total_miles_traveled = 0
+    user_thirst = 0
+    camel_tired = 0
+    distance_bluto_traveled = -20
+    cans_of_spinach = 3
+
+    done = False
+    while not done:
+        print("A. Eat more spinach.")
+        print("B. Ahead moderate speed.")
+        print("C. Ahead full speed.")
+        print("D. Stop for the night.")
+        print("E. Status check.")
+        print("Q. Quit.")
+        user_choice = input("What is your choice? ")
+
+        # Choice of quitting
+        if user_choice.upper() == "Q":
+            done = True
+            print("You have quit the best game ever.")
+
+        # Choice of status check
+        elif user_choice.upper() == "E":
+            print("Miles traveled:", user_total_miles_traveled)
+            print("Cans of spinach:", cans_of_spinach)
+            print("Bluto is", user_total_miles_traveled - distance_bluto_traveled, "miles behind you.")
+
+        # Choice of stop for the night
+        elif user_choice.upper() == "D":
+            import random
+            camel_tired = 0
+            print("Camel is happy!")
+            print("Popeye is rested!")
+            bluto_traveled_at_night = random.randrange(7, 15)
+            distance_bluto_traveled = distance_bluto_traveled + bluto_traveled_at_night
+
+        # Choice of full speed
+        elif user_choice.upper() == "C":
+            import random
+            user_traveled_full_speed = random.randrange(10, 21)
+            print("You traveled", user_traveled_full_speed, "miles.")
+            user_total_miles_traveled = user_total_miles_traveled + user_traveled_full_speed
+            user_thirst = user_thirst + 1
+            camel_tired_full_speed = random.randrange(1, 4)
+            camel_tired = camel_tired + camel_tired_full_speed
+            bluto_traveled_full_speed = random.randrange(7, 18)
+            distance_bluto_traveled = distance_bluto_traveled + bluto_traveled_full_speed
+
+        # Choice of moderate speed
+        elif user_choice.upper() == "B":
+            import random
+            user_traveled_moderate_speed = random.randrange(7, 13)
+            print("You traveled", user_traveled_moderate_speed, "miles.")
+            camel_tired = camel_tired + 1
+            user_thirst = user_thirst + 1
+            bluto_traveled_moderate_speed = random.randrange(7, 20)
+            user_total_miles_traveled = user_total_miles_traveled + user_traveled_moderate_speed
+            distance_bluto_traveled = distance_bluto_traveled + bluto_traveled_moderate_speed
+
+        # Choice of eating spinach
+        elif user_choice.upper() == "A":
+            if cans_of_spinach > 0:
+                cans_of_spinach = cans_of_spinach - 1
+                user_thirst = 0
+                print("Popeye enjoyed his spinach!")
+            elif cans_of_spinach <= 0:
+                print("No cans of spinach available!")
+
+        # Popeye is getting hungry
+        if not done and user_thirst > 4 and user_thirst <= 6:
+            print("You are hungry!")
+        elif not done and user_thirst > 6:
+            done = True
+            print("You died of hunger!")
+
+        # The camel is getting tired
+        elif not done and camel_tired > 5 and camel_tired <= 8:
+            print("Your camel is getting tired!")
+        elif not done and camel_tired > 8:
+            done = True
+            print("Your camel is dead!")
+
+        # Bluto is getting close to Popeye
+        elif not done and distance_bluto_traveled >= user_total_miles_traveled:
+            done = True
+            print("Bluto caught you and the camel!")
+        elif not done and distance_bluto_traveled >= (user_total_miles_traveled - 15):
+            print("Bluto is getting close!")
+
+        # The user won the game
+        elif not done and user_total_miles_traveled >= 200:
+            done = True
+            print("You successfully stole the camel and won the game!")
+
+
+# figure out how to break the code so that it doesn't give other statements after winning or losing
+# figure out the random chance
+
+main()
--- Testing/test.py
+++ Testing/test.py
@@ -1,8 +1,2 @@
-print("Most exciting class ever!!!")
-print("I want to sleep \\\\hi\\\\")
-print("What does\ndo? It\nis weird.")
-print("""Wow
-the professor
-has
-gone
-crazy""")
+a = 3 < 4
+print(a)
