869fb1f4514d6aff2f5664e0c5aee5e04ec59696
Christina Dietrich <christina.dietrich@my.simpson.edu>
2021-09-23 14:09:14 -0500

    Random Notes

--- Testing/test.py
+++ Testing/test.py
@@ -1,8 +1,25 @@
-print("Most exciting class ever!!!")
-print("I want to sleep \\\\hi\\\\")
-print("What does\ndo? It\nis weird.")
-print("""Wow
-the professor
-has
-gone
-crazy""")
+# print("Most exciting class ever!!!")
+# print("I want to sleep \\\\hi\\\\")
+# print("What does\ndo? It\nis weird.")
+# print("""Wow
+# the professor
+# has
+# gone
+# crazy""")
+
+
+# ch 11
+# 'for loops' - when you know how many times to loop
+# 'while loop' - loop until a condition
+
+# i represent increment, can be replaced with any variable
+
+# sentinel variable, teh variable watched until exiting a function
+# Running total
+# Boolean variables are true and false
+
+
+import random
+
+my_number = random.random()
+print(my_number * 9 + 1)
