cdd2aa02ec75f09c464d5cf59b8e2d8b51ed1845
Christina Dietrich <christina.dietrich@my.simpson.edu>
2021-09-25 13:01:11 -0500

    Work on notes

--- Testing/test.py
+++ Testing/test.py
@@ -1,13 +1,6 @@
-<<<<<<< HEAD
-a = 3 < 4
-print(a)
-=======
-print("Most exciting class ever!!!")
-print("I want to sleep \\\\hi\\\\")
-print("What does\ndo? It\nis weird.")
-print("""Wow
-the professor
-has
-gone
-crazy""")
->>>>>>> be00a580e19b2022cfdc7c54398c311f099331cf
+def volume_cylinder(radius, height):
+    pi = 3.141592653589
+    volume = pi * radius ** 2 * height
+    return volume
+
+six_pack_volume = volume_cylinder(2.5, 6) * 6
