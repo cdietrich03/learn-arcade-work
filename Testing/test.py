b9d1d823b4f4638933acf614507307d86c53cbd7
Christina Dietrich <christina.dietrich@my.simpson.edu>
2021-10-07 11:07:54 -0500

    Work on lab 6

--- Testing/test.py
+++ Testing/test.py
@@ -16,7 +16,21 @@
 
 # i represent increment, can be replaced with any variable
 
+
 # sentinel variable, teh variable watched until exiting a function
 # Running total
 # Boolean variables are true and false
 
+
+# Write your function below:
+def zap_list(list):
+    for index in range(len(my_list)):
+        my_list[index] = 0
+
+
+# This is some code you can use to test:
+
+# Example 1, should print [0, 0, 0, 0]
+my_list = [6, 1, -1, 10]
+zap_list(my_list)
+print("Example 1:", my_list)
