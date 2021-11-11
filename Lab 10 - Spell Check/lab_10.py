6fa4b5e5f7e328cec77f30c680b6f0995ae05ae4
Christina Dietrich <christina.dietrich@my.simpson.edu>
2021-11-11 15:33:34 -0600

    Work on lab 19

--- Lab 10 - Spell Check/lab_10.py
+++ Lab 10 - Spell Check/lab_10.py
@@ -1,51 +1,73 @@
 import re
 
-# This function takes in a line of text and returns
-# a list of words in the line.
+
+# Code to split the text into lines
 def split_line(line):
     return re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
 
 
 def main():
-    dictionary_list = open("dictionary.txt")
+    # Turning the dictionary into a list
+    my_file = open("dictionary.txt")
 
-    name_list = []
+    dictionary_list = []
 
-    for line in dictionary_list:
+    for line in my_file:
         line = line.strip()
-        name_list.append(line)
+        dictionary_list.append(line)
 
-    dictionary_list.close()
+    # Always close the file
+    my_file.close()
+
+    # Use the linear search to find words not in dictionary
     print("--- Linear Search ---")
 
     alice_text = open("AliceInWonderLand200.txt")
-    current_line = 0
-    word = 0
+    line_number = 0
+
+    # Loop through each line in the alice text
     for line in alice_text:
-        split_line(line)
-        word_list = re.findall('[A-Za-z]+(?:\'[A-Za-z]+)?', line)
-        line = line.strip()
+        word_list = split_line(line)
+        line_number += 1
+
+        # Loop through each word in the word list
         for word in word_list:
-            while word < len(word_list):
-                if dictionary_list != word.upper():
-                    print(word)
+            position = 0
+            while position < len(dictionary_list) and dictionary_list[position] != word.upper():
+                position += 1
+            if position == len(dictionary_list):
+                print("Line", line_number, "possible misspelled word:", word)
 
+    alice_text.close()
 
+    print()
+    print("--- Binary Search ---")
 
+    alice_text = open("AliceInWonderLand200.txt")
+    line_number = 0
 
-        # for word in word_list:
-        #     if position < len(word_list):
-        #         line += word
-        #     while current_position < len(word_list):
-        #         current_position += 1
-        #         if dictionary_list != word.upper():
-        #
+    # Loop through each line in the alice text
+    for line in alice_text:
+        word_list = split_line(line)
+        line_number += 1
 
+        # Loop through each word in the word list
+        for word in word_list:
+            lower_bound = 0
+            upper_bound = len(dictionary_list) - 1
+            found = False
+            while lower_bound <= upper_bound and not found:
+                middle_pos = (lower_bound + upper_bound) // 2
+                if dictionary_list[middle_pos] < word.upper():
+                    lower_bound = middle_pos + 1
+                elif dictionary_list[middle_pos] > word.upper():
+                    upper_bound = middle_pos - 1
+                else:
+                    found = True
+            if not found:
+                print("Line", line_number, "possible misspelled word:", word)
 
-
-main()
-
-
+    alice_text.close()
 
 
 main()
