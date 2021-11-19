afa8e3f8a9f511ddc294b3f660ef41ba6ac2b38e
Christina Dietrich <christina.dietrich@my.simpson.edu>
2021-11-19 12:20:15 -0600

    work on notes

--- Testing/test.py
+++ Testing/test.py
@@ -42,10 +42,38 @@
 
 # Import functions into 'my_functions' namespace
 
+# Start - sorting wks example - selection sort
+# 15 57 14 33 72 79 26 56 42 40
+
+# 14 is smallest, swap 14 to pos 0
+# 14 57 33 72 79 26 56 42 40
+
+# 15 is next smallest, swap 15 to pos 1
+# 14 15 57 33 72 79 26 56 42 40
+
+# 26 is next smallest, swap 26 to pos 2
+# 14 15 26 33 72 79 57 56 42 40
+
+# n = 10, 10 * 5 = 50
+# n = 100, 100 * 50 = 5000
+# n = 1000, 1000 * 500 = 500000
+# n * (n/2) = n^2 / 2
+
+
+# Take the first value, loop through, find the next number thats lower and flip them,
+# do the same thing with the second value and so on
+def selection_sort(my_list):
+    for cur_pos in range(len(my_list)):
+        min_pos = cur_pos
+        for scan_pos in range(cur_pos + 1, len(my_list)):
+            if my_list[scan_pos] < my_list[min_pos]:
+                min_pos = scan_pos
+
+        temp = my_list[min_pos]
+        my_list[min_pos] = my_list[cur_pos]
+        my_list[cur_pos] = temp
+
+
 my_list = [15, 57, 14, 33, 72, 79, 26, 56, 42, 40]
-print(my_list)
-# Swap the 15 and 14 values
-temp = my_list[0]
-my_list[0] = my_list[2]
-my_list[2] = temp
+selection_sort(my_list)
 print(my_list)
