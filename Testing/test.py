e89ea606231a3aa59b70d3a9c5c32dcce8079fe0
Christina Dietrich <christina.dietrich@my.simpson.edu>
2021-11-22 11:47:00 -0600

    work on notes

--- Testing/test.py
+++ Testing/test.py
@@ -60,20 +60,4 @@
 # n * (n/2) = n^2 / 2
 
 
-# Take the first value, loop through, find the next number thats lower and flip them,
-# do the same thing with the second value and so on
-def selection_sort(my_list):
-    for cur_pos in range(len(my_list)):
-        min_pos = cur_pos
-        for scan_pos in range(cur_pos + 1, len(my_list)):
-            if my_list[scan_pos] < my_list[min_pos]:
-                min_pos = scan_pos
 
-        temp = my_list[min_pos]
-        my_list[min_pos] = my_list[cur_pos]
-        my_list[cur_pos] = temp
-
-
-my_list = [15, 57, 14, 33, 72, 79, 26, 56, 42, 40]
-selection_sort(my_list)
-print(my_list)
