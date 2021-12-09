8584c2d66906fa24f415d5f2e36dabe6d094fbc7
Christina Dietrich <christina.dietrich@my.simpson.edu>
2021-12-09 12:08:25 -0600

    Work on final lab

--- Lab 12 - Final Lab/part_12.py
+++ Lab 12 - Final Lab/part_12.py
@@ -7,11 +7,7 @@ SPRITE_SCALING = 0.5
 
 DEFAULT_SCREEN_WIDTH = 600
 DEFAULT_SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Sprite Move with Scrolling Screen Example"
-
-# How many pixels to keep as a minimum margin between the character
-# and the edge of the screen.
-VIEWPORT_MARGIN = 100
+SCREEN_TITLE = "Alien Dash"
 
 # How fast the camera pans to the player. 1.0 is instant.
 CAMERA_SPEED = 0.1
@@ -19,57 +15,56 @@ CAMERA_SPEED = 0.1
 # How fast the character moves
 PLAYER_MOVEMENT_SPEED = 7
 
-TEXTURE_LEFT = 0
-TEXTURE_RIGHT = 1
-TEXTURE_STRAIGHT = 2
-BEE_COUNT = 10
-
-
-# class Menu(arcade.View):
+# Amount of bees spawned at one time
+BEE_COUNT = 7
 #
-#     def on_show(self):
-#         arcade.set_background_color(arcade.color.BANGLADESH_GREEN)
-#
-#     def on_draw(self):
-#         arcade.start_render()
-#         arcade.draw_text("Alien Dash", 300, 300, arcade.color.WHITE, 100)
-#         arcade.draw_text("Click for instructions")
-#
-#     def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
-#         instructions = Instructions()
-#         self.window.show_view(instructions)
+# done = False
 
 
+# Menu view for the screen
+class Menu(arcade.View):
+
+    def on_show(self):
+        arcade.set_background_color(arcade.color.BANGLADESH_GREEN)
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_text("Alien Dash", 130, 300, arcade.color.WHITE, 50)
+        arcade.draw_text("Click for instructions", 190, 270, arcade.color.WHITE_SMOKE, 15)
+
+    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
+        # When the screen is pressed, move to instruction screen
+        instructions = Instructions()
+        self.window.show_view(instructions)
+
+
+# Instructions for the game
 class Instructions(arcade.View):
     def on_show(self):
         arcade.set_background_color(arcade.color.AIR_FORCE_BLUE)
-
         arcade.set_viewport(0, self.window.width, 0, self.window.height)
 
     def on_draw(self):
         arcade.start_render()
-        arcade.draw_text("HI", 300, 300, arcade.color.BLACK, 100)
+        arcade.draw_text("Instructions", 140, 400, arcade.color.BLACK, 50)
+        arcade.draw_text("Jump to the top of the game in as quickly as possible.", 60, 300, arcade.color.DARK_GRAY, 15)
+        arcade.draw_text("Do not touch any bees along the way.", 120, 275, arcade.color.DARK_GRAY, 15)
+        arcade.draw_text("As time continues, more bees will spawn.", 105, 250, arcade.color.DARK_GRAY, 15)
+        arcade.draw_text("Use the arrow keys to move around.", 125, 225, arcade.color.DARK_GRAY, 15)
+        arcade.draw_text("Hit the gold star in order to win the game.", 105, 200, arcade.color.DARK_GRAY, 15)
 
     def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
+        # After click, move to the game view
         game = MyGame()
         game.setup()
         self.window.show_view(game)
 
 
+# Create the bee sprite
 class Bee(arcade.Sprite):
     def __init__(self, filename, sprite_scaling):
 
         super().__init__(filename, sprite_scaling)
-        # self.scale = 0.2
-        # self.textures = []
-        #
-        # texture = arcade.load_texture("bee.png", 0.2)
-        # self.textures.append(texture)
-        #
-        # texture = arcade.load_texture("bee.png", 0.2, flipped_horizontally=True)
-        # self.textures.append(texture)
-        #
-        # self.texture = texture
 
         self.change_x = 0
         self.change_y = 0
@@ -78,26 +73,11 @@ class Bee(arcade.Sprite):
         self.center_x += self.change_x
         self.center_y += self.change_y
 
-        # if self.change_x < 0:
-        #     self.texture = self.textures[TEXTURE_LEFT]
-        # elif self.change_x > 0:
-        #     self.texture = self.textures[TEXTURE_RIGHT]
 
-        # If we are out-of-bounds, then 'bounce'
-        # if self.left < 0:
-        #     self.change_x *= -1
-        #
-        # if self.right > 1856:
-        #     self.change_x *= -1
-
-
+# The game view
 class MyGame(arcade.View):
-    """ Main application class. """
 
     def __init__(self):
-        """
-        Initializer
-        """
         super().__init__()
 
         # Sprite lists
@@ -105,9 +85,9 @@ class MyGame(arcade.View):
         self.wall_list = None
         self.star_list = None
         self.bee_list = None
-        self.score = 0
         self.game_over = False
 
+        # For the timer
         self.total_time = 0.0
         self.output = "00:00:00"
 
@@ -125,13 +105,13 @@ class MyGame(arcade.View):
         self.camera_gui = arcade.Camera(DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT)
 
     def setup(self):
-        """ Set up the game and initialize the variables. """
         # Sprite lists
         self.player_list = arcade.SpriteList()
         self.wall_list = arcade.SpriteList()
         self.star_list = arcade.SpriteList()
         self.bee_list = arcade.SpriteList()
 
+        # The total time
         self.total_time = 0.0
 
         # Set up the player, image from kenney.nl
@@ -140,17 +120,18 @@ class MyGame(arcade.View):
         self.player_sprite.center_y = 100
         self.player_list.append(self.player_sprite)
 
+        # Load the map from tiled and add them to the wall list
         map_name = "Final.json"
-
         self.tile_map = arcade.load_tilemap(map_name, scaling=SPRITE_SCALING)
-
         self.wall_list = self.tile_map.sprite_lists["walls"]
 
-        star = arcade.Sprite("star.png", 0.2)
+        # set up star, image from kenney.nl
+        star = arcade.Sprite("star.png", 0.4)
         star.center_x = 1470
         star.center_y = 1750
         self.star_list.append(star)
 
+        # Use the background color of the tile map
         if self.tile_map.background_color:
             arcade.set_background_color(self.tile_map.background_color)
 
@@ -158,12 +139,11 @@ class MyGame(arcade.View):
                                                              self.wall_list,
                                                              gravity_constant=0.5)
 
-        print(len(self.bee_list))
+    # Do not show the mouse
+    def on_show(self):
+        self.window.set_mouse_visible(False)
 
     def on_draw(self):
-        """
-        Render the screen.
-        """
 
         # This command has to happen before we start drawing
         arcade.start_render()
@@ -180,12 +160,17 @@ class MyGame(arcade.View):
         # Select the (unscrolled) camera for our GUI
         self.camera_gui.use()
 
+        # Show the timer on the screen
         arcade.draw_text(self.output, 485, 570,
                          arcade.color.WHITE, 20)
 
-    def on_key_press(self, key, modifiers):
-        """Called whenever a key is pressed. """
+        # Show the amount of bees on the screen
+        output = f"Bees on screen: {len(self.bee_list)}"
+        arcade.draw_text(output, 10, 20, arcade.color.WHITE, 12)
 
+    def on_key_press(self, key, modifiers):
+
+        # How high the sprite can jump when the up key is pressed
         if key == arcade.key.UP:
             if self.physics_engine.can_jump():
                 self.player_sprite.change_y = 12
@@ -196,7 +181,7 @@ class MyGame(arcade.View):
             self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
 
     def on_key_release(self, key, modifiers):
-        """Called when the user releases a key. """
+        # Make the game less touchy
 
         if key == arcade.key.UP or key == arcade.key.DOWN:
             self.player_sprite.change_y = 0
@@ -204,15 +189,13 @@ class MyGame(arcade.View):
             self.player_sprite.change_x = 0
 
     def on_update(self, delta_time):
-        """ Movement and game logic """
 
-        # Call update on all sprites (The sprites don't do much in this
-        # example though.)
-
+        # If the game is not over, keep spawning new bees
         if not self.game_over:
             self.spawn_new_enemy_timer += delta_time
 
-            if self.spawn_new_enemy_timer > 15:
+            # Spawn 7 new bees every 3 seconds
+            if self.spawn_new_enemy_timer > 3:
                 self.spawn_new_enemy_timer = 0
 
                 for i in range(BEE_COUNT):
@@ -234,25 +217,44 @@ class MyGame(arcade.View):
                         bee_hit_list = arcade.check_for_collision_with_list(bee, self.bee_list)
 
                         if len(wall_hit_list) == 0 and len(bee_hit_list) == 0:
-                            # It is!
                             bee_placed_successfully = True
+
                     self.bee_list.append(bee)
 
             self.physics_engine.update()
 
+            # Keep the timer going
             self.total_time += delta_time
 
+            # Calculate the time on the clock
             minutes = int(self.total_time) // 60
             seconds = int(self.total_time) % 60
             hundreths = int((self.total_time - seconds) * 100)
 
+            # The clock all put together
             self.output = f"{minutes:02d}:{seconds:02d}:{hundreths:02d}"
 
+            # If the player collides with a bee, send to the game over screen
             if len(arcade.check_for_collision_with_list(self.player_sprite, self.bee_list)) > 0:
                 self.game_over = True
+                game_over_view = GameOver()
+                game_over_view.total_time = self.total_time
+                game_over_view.bee_list = self.bee_list
+                self.window.set_mouse_visible(True)
+                self.window.show_view(game_over_view)
 
             self.bee_list.update()
 
+            # If the player collides with the star, send to the win screen
+            if len(arcade.check_for_collision_with_list(self.player_sprite, self.star_list)) > 0:
+                self.game_over = True
+                win_view = WinView()
+                win_view.total_time = self.total_time
+                win_view.bee_list = self.bee_list
+                self.window.set_mouse_visible(True)
+                self.window.show_view(win_view)
+
+            # The boundaries of the bee and reversing
             for bee in self.bee_list:
                 # If the enemy hit a wall, reverse
                 if len(arcade.check_for_collision_with_list(bee, self.wall_list)) > 0:
@@ -268,30 +270,83 @@ class MyGame(arcade.View):
             self.scroll_to_player()
 
     def scroll_to_player(self):
-        """
-        Scroll the window to the player.
-        if CAMERA_SPEED is 1, the camera will immediately move to the desired position.
-        Anything between 0 and 1 will have the camera move to the location with a smoother
-        pan.
-        """
 
+        # Scroll the screen around the player
         position = self.player_sprite.center_x - DEFAULT_SCREEN_WIDTH / 2, \
             self.player_sprite.center_y - DEFAULT_SCREEN_HEIGHT / 2
         self.camera_sprites.move_to(position, CAMERA_SPEED)
 
     def on_resize(self, width, height):
-        """
-        Resize window
-        Handle the user grabbing the edge and resizing the window.
-        """
+
         self.camera_sprites.resize(int(width), int(height))
         self.camera_gui.resize(int(width), int(height))
 
 
+# When the game is over, show this screen
+class GameOver(arcade.View):
+    def __init__(self):
+        super().__init__()
+        self.total_time = 0
+        self.bee_list = 0
+
+    def on_show(self):
+        arcade.set_background_color(arcade.color.BRICK_RED)
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_text("GAME OVER", 100, 400, arcade.color.WHITE, 50)
+        arcade.draw_text("Click to play again", 210, 375, arcade.color.WHITE, 15)
+
+        time_format = f"{round(self.total_time, 2)} seconds"
+        arcade.draw_text(f"Time: {time_format}", 210, 225, arcade.color.WHITE, 15)
+
+        output_bees = f"Bees on screen: {len(self.bee_list)}"
+        arcade.draw_text(output_bees, 215, 200, arcade.color.WHITE, 15)
+
+    # When the screen is clicked, move back to the game
+    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
+        game = MyGame()
+        game.setup()
+        self.window.show_view(game)
+
+    # def on_key_press(self, key, modifiers):
+        # if key == arcade.key.SPACE:
+        #     done = True
+        #
+
+
+# Show this screen when the player wins
+class WinView(arcade.View):
+    def __init__(self):
+        super().__init__()
+        self.total_time = 0
+        self.bee_list = 0
+
+    def on_show(self):
+        arcade.set_background_color(arcade.color.GOLD)
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_text("YOU WIN!", 140, 400, arcade.color.WHITE, 50)
+        arcade.draw_text("Click to play again", 215, 375, arcade.color.WHITE, 15)
+
+        time_format = f"{round(self.total_time, 2)} seconds"
+        arcade.draw_text(f"Time: {time_format}", 210, 225, arcade.color.WHITE, 15)
+
+        output_bees = f"Bees on screen: {len(self.bee_list)}"
+        arcade.draw_text(output_bees, 210, 200, arcade.color.WHITE, 15)
+
+    # When the screen is pressed, play the game again
+    def on_mouse_press(self, x: float, y: float, button: int, modifiers: int):
+        game = MyGame()
+        game.setup()
+        self.window.show_view(game)
+
+
 def main():
     """ Main function """
-    window = arcade.Window(DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT, "Alien Dash")
-    start_view = Instructions()
+    window = arcade.Window(DEFAULT_SCREEN_WIDTH, DEFAULT_SCREEN_HEIGHT, SCREEN_TITLE)
+    start_view = Menu()
     window.show_view(start_view)
     arcade.run()
 
